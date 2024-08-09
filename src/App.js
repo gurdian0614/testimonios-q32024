ae2b2f932c2994d2639fc7c139522f1dd3187e58
gurdian0614 <ivan.gurdian@outlook.com>
2024-08-08 20:36:05 -0600

    Añadiendo css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Testimonio from './components/Testimonio';
 function App() {
   return (
     <div className="App">
-      <div className='contenedor-principal'>
+      <div className='contenedorPrincipal'>
         <h1>Esto es lo que dicen los estudiantes de CEUTEC:</h1>
         <Testimonio
           nombre='Roger Gurdian'
@@ -12,6 +12,14 @@ function App() {
           cargo='Fullstack Developer'
           empresa='HouseHold Solutions'
           testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero la clase de programación web I fue donde aprendí. Estudiar JavaScript, así como estructuras de datos y algoritmos en la clase me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+        />
+
+        <Testimonio
+          nombre='Juan Perez'
+          lugar='Santa Bárbara'
+          cargo='Ingeniero de Software'
+          empresa='spotify'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero la clase de programación web I fue donde aprendí. Estudiar JavaScript, así como estructuras de datos y algoritmos en la clase me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify. Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero la clase de programación web I fue donde aprendí.'
         />
       </div>
     </div>
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -3,12 +3,12 @@ import imagen from '../images/testimonio-shawn.png'
 
 function Testimonio(props) {
     return(
-        <div className='contenedor-testimonio'>
-            <img className='imagen-testimonio' src={imagen} alt='Imagen' />
-            <div className='contenedor-text-testimonio'>
-                <p className='nombre-testimonio'>{props.nombre} de {props.lugar}</p>
-                <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
-                <p className='texto-testimonio'>{props.testimonio}</p>
+        <div className='contenedorTestimonio'>
+            <img className='imagenTestimonio' src={imagen} alt='Imagen' />
+            <div className='contenedorTextoTestimonio'>
+                <p className='nombreTestimonio'>{props.nombre} de {props.lugar}</p>
+                <p className='cargoTestimonio'>{props.cargo} en {props.empresa}</p>
+                <p className='textoTestimonio'>{props.testimonio}</p>
             </div>
         </div>
     )
